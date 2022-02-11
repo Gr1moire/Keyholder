df5eb58f910ab69dc76d0349ac45aff290d82506
Nolan <nolan.kijas@epitech.eu>
2022-02-11 21:16:11 +0100

    add player bullet and first obstcle

---
+++ player/Player.gd
@@ -0,0 +1,41 @@
+extends KinematicBody2D
+
+export var MAX_SPEED = 400
+export var ACCELERATION = 8000
+export var FRICTION = 2000
+export var bullet_speed = 1000
+export var fire_rate = 0.2
+
+var bullet = preload("res://player/Bullet.tscn")
+var can_fire = true
+
+var velocity = Vector2.ZERO
+
+func _process(delta):
+	look_at(get_global_mouse_position())
+	
+	if Input.is_action_pressed("fire") and can_fire:
+		var bullet_instance = bullet.instance()
+		bullet_instance.position = $BulletPoint.global_position
+		bullet_instance.rotation_degrees = rotation_degrees
+		bullet_instance.apply_impulse(Vector2(), Vector2(bullet_speed, 0).rotated(rotation))
+		get_tree().get_root().add_child(bullet_instance)
+		can_fire = false
+		yield(get_tree().create_timer(fire_rate), "timeout")
+		can_fire = true
+
+func _physics_process(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("right") - Input.get_action_strength("left")
+	input_vector.y = Input.get_action_strength("down") - Input.get_action_strength("up")
+	input_vector = input_vector.normalized()
+	
+	if input_vector != Vector2.ZERO:
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION *delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)	
+
+	move()
+
+func move():
+	velocity = move_and_slide(velocity)
