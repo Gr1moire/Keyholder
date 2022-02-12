11b14789104ac19cba5bea20e55df742b7e0fb19
Michaël Veillet Guillem <micou.veillet@gmail.com>
2022-02-12 13:04:56 +0100

    key: Implement multiple group to attach to

--- common/cle/Cle.gd
+++ common/cle/Cle.gd
@@ -2,7 +2,7 @@ extends "res://common/smooth_movement.gd"
 
 # Declare member variables here. Examples:
 var attached_to: Node2D = null
-export(Array) var group_to_attach_to = ["Player"]
+export(Array, String) var group_to_attach_to = ["Player"]
 
 ## Called when the node enters the scene tree for the first time.
 func _ready():
@@ -17,6 +17,7 @@ func transfer_ownership(body: Node2D):
 	attached_to = body
 
 func _on_RigidBody2D_body_entered(body: Node2D):
-	if body.is_in_group("Player"):
-		self.transfer_ownership(body)
-		self.move(attached_to.position)
+	for group in group_to_attach_to:
+		if body.is_in_group(group):
+			self.transfer_ownership(body)
+			self.move(attached_to.position)
