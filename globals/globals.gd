18d562f4fc89c57473fd2d370a55738ef26fc268
Nolan <nolan.kijas@epitech.eu>
2022-02-14 10:23:17 +0100

    flee point for lvl 3 and assets for player

--- globals/globals.gd
+++ globals/globals.gd
@@ -4,3 +4,29 @@ extends Node
 var key
 var camera
 var player
+
+const run_point = [	Vector2(105, 105),
+					Vector2(490, 105),
+					Vector2(950, 105),
+					Vector2(950, 300),
+					Vector2(950, 520),
+					Vector2(500, 520),
+					Vector2(100, 520),
+					Vector2(100, 300)]
+
+const run_point_2 = [	Vector2(72, 268),
+						Vector2(257, 129),
+						Vector2(370, 413),
+						Vector2(672, 409),
+						Vector2(801, 126),
+						Vector2(950, 294),
+						Vector2(526, 106)]
+
+const run_point_3= [	Vector2(125, 160),
+						Vector2(525, 150),
+						Vector2(900, 150),
+						Vector2(960, 310),
+						Vector2(900, 500),
+						Vector2(530, 500),
+						Vector2(170, 500),
+						Vector2(90, 130)]
