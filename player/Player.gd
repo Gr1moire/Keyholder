19bc4aa679296f36ba8362852cf1b54ffaeb2723
Nolan <nolan.kijas@epitech.eu>
2022-02-14 11:40:57 +0100

    missing part and add moving sprite

--- player/Player.gd
+++ player/Player.gd
@@ -3,34 +3,19 @@ extends KinematicBody2D
 export var MAX_SPEED = 300
 export var ACCELERATION = 6000
 export var FRICTION = 2000
-export var bullet_speed = 1000
-export var fire_rate = 0.2
 var canInteract = false;
-
-var bullet = preload("res://player/Bullet.tscn")
-var can_fire = true
 
 var velocity = Vector2.ZERO
 var key
 var has_key: bool = false
 
+var x_percent
+var mouse_position
+var screen_width
+
 func _ready():
 	key = Globals.key
 	Globals.player = self;
-
-func _process(_delta):
-	look_at(get_global_mouse_position())
-	
-	if Input.is_action_pressed("fire") and can_fire:
-		Globals.camera.shake(0.1, 15, 2)
-		var bullet_instance = bullet.instance()
-		bullet_instance.position = $BulletPoint.global_position
-		bullet_instance.rotation_degrees = rotation_degrees
-		bullet_instance.apply_impulse(Vector2(), Vector2(bullet_speed, 0).rotated(rotation))
-		get_tree().get_root().add_child(bullet_instance)
-		can_fire = false
-		yield(get_tree().create_timer(fire_rate), "timeout")
-		can_fire = true
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
@@ -43,8 +28,26 @@ func _physics_process(delta):
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)	
 
+	handle_mouse(input_vector)
 	move()
 
+func handle_mouse(input):
+	mouse_position = get_viewport().get_mouse_position()
+	screen_width = get_viewport().size.x / 4
+	print(input)
+	if mouse_position.x > screen_width:
+		if input.x != 0 or input.y != 0:
+			$AnimatedSprite.play("RIGHTWALK")
+		elif input == Vector2.ZERO:
+			$AnimatedSprite.play("RIGHT")
+			
+	else:
+		if input.x != 0 or input.y != 0:
+			$AnimatedSprite.play("LEFTWALK")
+		elif input == Vector2.ZERO:
+			$AnimatedSprite.play("LEFT")
+
+		
 func move():
 	velocity = move_and_slide(velocity)
 
